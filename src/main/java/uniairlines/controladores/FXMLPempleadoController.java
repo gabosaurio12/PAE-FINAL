654577d9e353ee911cb707fc5106308226709c18
SnakeGuitar <snakersito08@gmail.com>
2025-06-07 21:28:28 -0600

    Update FXMLPempleadoController.java
    Vinculación con ventanas de clientes

--- src/main/java/uniairlines/controladores/FXMLPempleadoController.java
+++ src/main/java/uniairlines/controladores/FXMLPempleadoController.java
@@ -4,29 +4,45 @@
  */
 package uniairlines.controladores;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import uniairlines.UNIAIRLINES;
 import uniairlines.modelo.Usuario;
+import uniairlines.modelo.pojo.boleto.Cliente;
+import uniairlines.util.ResultadoFXML;
+import uniairlines.util.utilgeneral;
 
 /**
  *
  * @author cuent
  */
 public class FXMLPempleadoController implements Initializable {
+    private final String RUTA_FXML_REGISTRAR_CLIENTE = "resources/vista/FXMLRegistrarClientes.fxml";
+    private final String RUTA_FXML_CONSULTAR_CLIENTES = "resources/vista/FXMLConsultarClientes.fxml";
+
+    private final utilgeneral util = new utilgeneral();
 
     @FXML
     private AnchorPane Aerolinea;
     @FXML
     private Label lbAerolinea;
-    
+    @FXML
     private static String seleccionada;
     
-     private static Label lbAerolineaStatic;
+    private static Label lbAerolineaStatic;
     /**
      * Initializes the controller class.
      */
@@ -44,6 +60,16 @@ public class FXMLPempleadoController implements Initializable {
 
     @FXML
     private void lbAerolinea(MouseEvent event) {
+
     }
- 
+
+    @FXML
+    public void buttonConsultarLista(ActionEvent actionEvent) {
+        utilgeneral.crearEscenarioSimple(RUTA_FXML_CONSULTAR_CLIENTES, "Consultar Clientes");
+    }
+
+    @FXML
+    public void buttonRegistrarCliente(ActionEvent actionEvent) {
+        utilgeneral.crearEscenarioSimple(RUTA_FXML_REGISTRAR_CLIENTE, "Registrar Cliente");
+    }
 }
